aba9f1b37ad562a69b619da051a73224ed519f2b
tangqi <793672954@qq.com>
2021-03-04 21:50:29 +0800

    firsttime

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    Home
-    <el-button>默认按钮</el-button>
+  <div class="cz-main">
+    <section class="homewrap">
+      <Bannerswiper />
+      <Nav />
+      <Hotgoods />
+    </section>
   </div>
 </template>
 <script>
+import Bannerswiper from "./Bannerswiper/index";
+import Nav from "./Nav/index";
+import Hotgoods from "./Hotgoods/index"
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  components: {
+    Bannerswiper,
+    Nav,
+    Hotgoods
+  },
+};
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
+.cz-main {
+  max-width: 1220px;
+  min-height: calc(100vh - 320px);
+  margin: 0 auto;
+  .homewrap{
+    padding: 25px 0;
+  }
+}
 </style>
---
+++ src/pages/Home/Nav/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="Navwrap">
+    <figure>
+      <img src="./images/fbefae00ef98859f342e9f56889899a0.webp" alt="" />
+    </figure>
+    <figure>
+      <img src="./images/1023fe9691eaf03abbfd261b36df4985.webp" alt="" />
+    </figure>
+    <figure>
+      <img src="./images/33ea4bfa05068e0741b7278fec9da8a6.webp" alt="" />
+    </figure>
+    <figure>
+      <img src="./images/fdc4370d1ce14a67fadc35d74209ac0f.webp" alt="" />
+    </figure>
+  </section>
+</template>
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+.Navwrap {
+  height: 200px;
+  width: 100%;
+  display: flex;
+  margin-top:25px;
+  border-radius: 8px;
+  overflow: hidden;
+  figure {
+    margin: 0;
+    img {
+      width: 100%;
+      cursor: pointer;
+    }
+  }
+}
+</style>
