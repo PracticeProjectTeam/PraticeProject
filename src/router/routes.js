947922b27b3498969533872a98c5131552b5448d
luoxiaoqin <2522041222@qq.com>
2021-03-08 20:12:54 +0800

    3/8

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,8 +2,6 @@
 const Home=()=>import('@/pages/Home/Home.vue') // 按需加载
 const Order=()=>import('@/pages/order/order.vue') // 按需加载
 const Goodlist=()=>import('@/pages/Goodlist/Goodlist.vue') // 按需加载
-//const AfterSale=()=>import('@/pages/order/afterSale/afterSale.vue') // 按需加载
-//const Particulars=()=>import('@/pages/order/particulars/particulars.vue') // 按需加载
 const WriteOrder=()=>import('@/pages/order/writeOrder/writeOrder.vue') // 按需加载
 
 export default [
@@ -17,18 +15,6 @@ export default [
   {
     path:'/order',
     component:Order,
-    children:[
-      /* 售后服务 */
-      // {
-      //   path:'/order/afterSale',
-      //   component:AfterSale
-      // },
-      /* 填写订单 */
-      // {
-      //   path:'/order/writeOrder',
-      //   component:WriteOrder
-      // }
-    ]
   },
   // 商品列表
   {
